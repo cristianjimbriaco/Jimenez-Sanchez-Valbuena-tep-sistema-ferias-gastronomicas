25186bccb48289ecdd408ee4d783b0ed8bf06413
cristianjimbriaco <cjjimenez.23@est.ucab.edu.ve>
2026-01-10 21:33:24 -0400

    feat 1.2: Terminar de crear la entidad user

---
+++ ms-users-auth/src/users/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
+import { UserRole } from '../enums/user-role.enum';
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 100 })
+    fullName: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({   
+        type: 'enum',
+        enum: UserRole,
+    })
+    role: UserRole;
+
+    @Column({ default: true })
+    activo: boolean;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: Date;
+
+    @DeleteDateColumn({ name: 'deleted_at' })
+    deletedAt?: Date;
+}
