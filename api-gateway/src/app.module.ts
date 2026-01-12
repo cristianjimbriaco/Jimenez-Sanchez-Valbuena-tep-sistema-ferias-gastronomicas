1dc7b7b2c4397ab4106442c03ca1240d12be0a96
cristianjimbriaco <cjjimenez.23@est.ucab.edu.ve>
2026-01-12 12:26:55 -0400

    feat 8.1: Configuracion del API Gateway, creacion de archivos y modificacion del main para no exponer RPC

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USERS_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: 'localhost',
+          port: 3001,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(3000);
+  console.log('API Gateway escuchando en http://localhost:3000');
 }
 bootstrap();
