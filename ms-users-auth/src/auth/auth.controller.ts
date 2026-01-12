4a66f2b53e554385e0c169d960e65da36087abd8
cristianjimbriaco <cjjimenez.23@est.ucab.edu.ve>
2026-01-12 11:15:05 -0400

    feature 2.7: Validacion de token para otros microservicios, falta probar cuando hayan otros microservicios

--- ms-users-auth/src/auth/auth.controller.ts
+++ ms-users-auth/src/auth/auth.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, 
+              private readonly jwtService: JwtService
+  ) {}
 
   @Post('login')
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto.email, dto.password);
+  }
+
+  // RPC para validar token
+  @MessagePattern({ cmd: 'validate_token' })
+  validateToken(@Payload() token: string) {
+    try {
+      return this.jwtService.verify(token);
+    } catch (error) {
+      throw new UnauthorizedException('Token inválido');
+    }
   }
 }
